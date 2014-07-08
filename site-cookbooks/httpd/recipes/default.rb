1d69808b68d841e4bc8e201e90f18c63b3228cba
takatsugu ishikawa <tissi0708@gmail.com>
2014-07-08 18:57:58 +0900

    add a cookbook of httpd

---
+++ site-cookbooks/httpd/recipes/default.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: httpd
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "httpd" do
+  action :install
+end
+
+service "httpd" do
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
+end
+
+template "httpd.conf" do
+    path "/etc/httpd/conf/httpd.conf"
+    source "httpd.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    notifies :reload, 'service[httpd]'
+end
+
+service 'iptables ' do
+  action [:disable , :stop]
+end
